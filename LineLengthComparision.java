6899ab2e0d302b6c8251f9852952ac6e7d42cd4a
AkshataHasabi <akshuh818@gmail.com>
2022-01-06 00:12:17 +0530

    ADDED:welcome message

---
+++ LineLengthComparision.java
@@ -0,0 +1,8 @@
+package com.bridgelab.linecompareoops;
+
+public class LineLengthComparision {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparision computation program");
+	}
+}
